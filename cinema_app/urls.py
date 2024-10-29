74c7372f02f3ae63c53d198721940971d1d3262e
Aluiel <heaven99902@gmail.com>
2024-10-29 09:26:51 +0200

    added password reset templates

--- cinema_app/urls.py
+++ cinema_app/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('accounts/password_change/', views.CustomPasswordChangeView.as_view(), name='password_change'),
     path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-    path('accounts/password_reset/s', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
          name='password_reset_confirm'),
